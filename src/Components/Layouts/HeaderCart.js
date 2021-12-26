0d8000ef90350f99e2333f5bf47a5a6d9ddbef66
sudhakar-dhayalan <sudhakar.dayalan95@gmail.com>
2021-12-26 12:51:00 +0530

    feat: added bump on add or remove item from cart

--- src/Components/Layouts/HeaderCart.js
+++ src/Components/Layouts/HeaderCart.js
@@ -1,16 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCart.module.css";
 
 const HeaderCart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isBtnHighlighted, setIsBtnHighlighted] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce((currNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+
+  const btnClass = `${classes.button} ${isBtnHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsBtnHighlighted(true);
+
+    const timer = setTimeout(() => setIsBtnHighlighted(false), 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClass} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
